08766e9b7f7d58f1c9098ef07b989fa6462ed2d7
AgentSA <esl.agentsa@gmail.com>
2026-03-09 22:16:20 +0100

    Feature: ajout logos Iconify pour tous les logiciels dans les comparatifs

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { r as renderers } from './chunks/_@astro-renderers_ZkvB7lbw.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_C-_sqSNC.mjs';
-import { manifest } from './manifest_CjwNzSLm.mjs';
+import { manifest } from './manifest_y_F_rkEE.mjs';
 
 const serverIslandMap = new Map();;
 
